ba267942341a47dcb0b2a6267f39d28b8e4bae3d
steigert <steigert@fe2ec71a-4f37-0410-9b5c-d1cd6552648d>
2007-12-04 13:47:54 +0000

    Adicionado simulador

--- src/main/java/br/pucsp/tcc/gui/gerarIdentificacao/impl/ExibicaoClienteGui__GUSTAVO.java
+++ src/main/java/br/pucsp/tcc/gui/gerarIdentificacao/impl/ExibicaoClienteGui__GUSTAVO.java
@@ -5,14 +5,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import br.pucsp.tcc.infra.leitorbiometrico.LeitorBiometrico;
 import br.pucsp.tcc.infra.leitorbiometrico.LeitorBiometricoListener;
-import br.pucsp.tcc.infra.leitorbiometrico.impl.LeitorMicrosoft;
+import br.pucsp.tcc.infra.leitorbiometrico.impl.LeitorSimulador;
 import br.pucsp.tcc.modelo.Identificacao;
 import br.pucsp.tcc.modelo.ImpressaoDigital;
 
@@ -62,10 +67,29 @@ public class ExibicaoClienteGui__GUSTAVO extends JFrame implements LeitorBiometr
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        this.addKeyListener(new KeyAdapter() {
+            final LeitorSimulador leitor = (LeitorSimulador) ExibicaoClienteGui__GUSTAVO.this.leitor;
+            @Override
+            public void keyReleased(KeyEvent e)
+            {
+                String key = String.valueOf(e.getKeyChar());
+                try
+                {
+                    Integer keyNum = Integer.parseInt(key);
+                    logger.trace(String.format("Simulador acionado, id '%s'", e.getKeyChar()));
+                    leitor.simular(keyNum);
+                }
+                catch(NumberFormatException e1)
+                {
+                }
+            }
+        });
 	}
 	
 	private void inicializarLeitor() {
-		leitor = new LeitorMicrosoft();
+		//leitor = new LeitorMicrosoft();
+		leitor = new LeitorSimulador();
 		try {
 			leitor.iniciar(this);
 		} catch (Exception e) {
@@ -75,12 +99,16 @@ public class ExibicaoClienteGui__GUSTAVO extends JFrame implements LeitorBiometr
 
 	public void leituraIdentificacaoFinalizada(Identificacao identificacao)
 	{
+	    logger.debug("Terminada captura de impressão digital");
 		this.identificacao = identificacao;
 		painelIdentificacao.setBackground(Color.WHITE);
 		painelIdentificacao.repaint();
 	}
 
 	public void leituraIdentificacaoIniciada() {
+	    logger.debug("Iniciando captura de impressão digital");
 		painelIdentificacao.setBackground(Color.LIGHT_GRAY);
 	}
+	
+	 static Log logger = LogFactory.getLog(ExibicaoClienteGui__GUSTAVO.class);
 }
